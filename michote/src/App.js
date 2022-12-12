24d6589b766df17a62a2d425a30834cf970dd357
mkoner <amzid@hotmail.fr>
2022-12-12 12:48:37 +0000

    resolving conflict

--- michote/src/App.js
+++ michote/src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
-      <Footer/>
     </div>
   );
 }
