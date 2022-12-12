7cbf8979a201c4acbb81ee087cc6cf42439dd2aa
mkoner <amzid@hotmail.fr>
2022-12-12 21:47:13 +0000

    resolving conflict

--- michote/src/App.js
+++ michote/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Home from './pages/home/home';
 import BookingPage from './pages/booking/booking';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
       <BookingPage/>
     </div>
   );
