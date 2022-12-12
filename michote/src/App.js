9b7a188af14bead7b2616dda5816a6a09a9ce333
mkoner <amzid@hotmail.fr>
2022-12-12 16:02:35 +0000

    added home page

--- michote/src/App.js
+++ michote/src/App.js
@@ -1,9 +1,14 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Header from './components/header/header';
+import Footer from './components/footer/footer';
+import Home from './pages/home/home';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
     </div>
   );
 }
