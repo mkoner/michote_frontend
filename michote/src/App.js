e15b58fb6878257413d83b5a0728158d7f4493fd
mkoner <amzid@hotmail.fr>
2022-12-12 12:43:25 +0000

    created footer component

--- michote/src/App.js
+++ michote/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
+      <Footer/>
     </div>
   );
 }
---
+++ michote/src/components/footer/footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+import './footer.css'
+
+export default function Footer() {
+  return (
+    <footer className='footer'>
+        <ul className='footer-items'>
+              <li><a href='/home'>Home</a></li>
+              <li><a href='/about'>About Us</a></li>
+              <li><a href='/partners'>Partners</a></li>
+              <li><a href='/contact'>Contact Us</a></li>
+        </ul>
+        <div className="credit">
+            <p>
+                Powered and <br/> Inspired by ALX
+            </p>
+        </div>
+    </footer>
+  )
+}
