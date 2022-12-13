e9c2f7bf94deca9e3a1939c141bd2d70ee5cf47f
mkoner <amzid@hotmail.fr>
2022-12-13 12:17:57 +0000

    partner register page

--- michote/src/App.js
+++ michote/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import PartnerRegister from './components/pages/partnerRegister/partnerRegister';
 
 function App() {
   return (
     <div className="App">
+      <PartnerRegister/>
     </div>
   );
 }
